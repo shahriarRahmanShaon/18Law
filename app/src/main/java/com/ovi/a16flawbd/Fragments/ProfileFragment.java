dd001af0c4f100e4ada635e3b429e9c7e018ac37
shahriarRahmanShaon <shaon463@gmail.com>
2021-02-18 06:57:47 +0600

    profile fragment complete

--- app/src/main/java/com/ovi/a16flawbd/Fragments/ProfileFragment.java
+++ app/src/main/java/com/ovi/a16flawbd/Fragments/ProfileFragment.java
@@ -2,22 +2,65 @@ package com.ovi.a16flawbd.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ovi.a16flawbd.ModelClasses.UserModel;
 import com.ovi.a16flawbd.R;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class ProfileFragment extends Fragment {
+    CircleImageView profileImage;
+    TextView username;
 
+    FirebaseUser firebaseUser;
+    DatabaseReference databaseReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profileImage = view.findViewById(R.id.profileImage);
+        username = view.findViewById(R.id.username);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("BaatCheet/Users/").child(firebaseUser.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserModel userModel = snapshot.getValue(UserModel.class);
+                username.setText(userModel.getUsername());
+                if (userModel.getImageURL().equals("default")) {
+                    Picasso.get().load(R.mipmap.ic_launcher).into(profileImage);
+                }
+                else {
+                    Picasso.get().load(userModel.getImageURL()).into(profileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 }
